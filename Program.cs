d8af67ff3bed63457b2fddd9293eea55f0c64562
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-08-25 12:03:48 -0500

    added some fun

--- Program.cs
+++ Program.cs
@@ -9,8 +9,46 @@ namespace guessingGame
         {
             int SecretNumber = new Random().Next(1, 100);
             int DifficultySelection = 0;
+            Console.WriteLine(@"
+            
+            Brandon Alexander Wheatley Presents...
 
-            Console.WriteLine("Welcome, traveller...are you feeling very easy, medium, or hard today? Also definitely do NOT type \"cheater\"");
+
+            ▄██████▄  ███    █▄     ▄████████    ▄████████    ▄████████  ▄█  ███▄▄▄▄      ▄██████▄       
+            ███    ███ ███    ███   ███    ███   ███    ███   ███    ███ ███  ███▀▀▀██▄   ███    ███      
+            ███    █▀  ███    ███   ███    █▀    ███    █▀    ███    █▀  ███▌ ███   ███   ███    █▀       
+           ▄███        ███    ███  ▄███▄▄▄       ███          ███        ███▌ ███   ███  ▄███             
+          ▀▀███ ████▄  ███    ███ ▀▀███▀▀▀     ▀███████████ ▀███████████ ███▌ ███   ███ ▀▀███ ████▄       
+            ███    ███ ███    ███   ███    █▄           ███          ███ ███  ███   ███   ███    ███      
+            ███    ███ ███    ███   ███    ███    ▄█    ███    ▄█    ███ ███  ███   ███   ███    ███      
+            ████████▀  ████████▀    ██████████  ▄████████▀   ▄████████▀  █▀    ▀█   █▀    ████████▀       
+                                                                                                            
+            ▄██████▄     ▄████████   ▄▄▄▄███▄▄▄▄      ▄████████                                          
+            ███    ███   ███    ███ ▄██▀▀▀███▀▀▀██▄   ███    ███                                          
+            ███    █▀    ███    ███ ███   ███   ███   ███    █▀                                           
+           ▄███          ███    ███ ███   ███   ███  ▄███▄▄▄                                              
+          ▀▀███ ████▄  ▀███████████ ███   ███   ███ ▀▀███▀▀▀                                              
+            ███    ███   ███    ███ ███   ███   ███   ███    █▄                                           
+            ███    ███   ███    ███ ███   ███   ███   ███    ███                                          
+            ████████▀    ███    █▀   ▀█   ███   █▀    ██████████                                          
+                                                                                                            
+            ▄██████▄     ▄████████                                                                         
+            ███    ███   ███    ███                                                                         
+            ███    ███   ███    █▀                                                                          
+            ███    ███  ▄███▄▄▄                                                                             
+            ███    ███ ▀▀███▀▀▀                                                                             
+            ███    ███   ███                                                                                
+            ███    ███   ███                                                                                
+            ▀██████▀    ███                                                                                
+                                                                                                            
+                ███        ▄█    █▄       ▄████████  ▄██████▄  ███▄▄▄▄      ▄████████    ▄████████          
+            ▀█████████▄   ███    ███     ███    ███ ███    ███ ███▀▀▀██▄   ███    ███   ███    ███          
+               ▀███▀▀██   ███    ███     ███    ███ ███    ███ ███   ███   ███    █▀    ███    █▀           
+                ███   ▀  ▄███▄▄▄▄███▄▄  ▄███▄▄▄▄██▀ ███    ███ ███   ███  ▄███▄▄▄       ███                 
+                ███     ▀▀███▀▀▀▀███▀  ▀▀███▀▀▀▀▀   ███    ███ ███   ███ ▀▀███▀▀▀     ▀███████████          
+                ███       ███    ███   ▀███████████ ███    ███ ███   ███   ███    █▄           ███          
+                ███       ███    ███     ███    ███ ███    ███ ███   ███   ███    ███    ▄█    ███          
+               ▄████▀     ███    █▀      ███    ███  ▀██████▀   ▀█   █▀    ██████████  ▄████████▀  ");
             Console.Beep(700, 1000);
             Console.Beep(450, 1000);
             Console.Beep(550, 150);
@@ -28,7 +66,22 @@ namespace guessingGame
             Console.Beep(420, 800);
             Console.Beep(550, 150);
             Console.Beep(520, 150);
-            Console.Beep(450, 800);
+            Console.Beep(450, 2000);
+            Console.WriteLine(@"From the outside it looks cheerless, dull and broken. Softwooden planks and wooden pillars make up most of the building's outer structure.
+It's difficult to see through the large, curtained windows, but the awkward silence from within can be felt outside.
+
+As you enter the tavern through the dirty, wooden door, you're welcomed by the smell of alcohol and thick air.
+The bartender is lying in a chair, doing nothing and makes no effort to acknowledge your pressence.
+
+It's as horrible inside as it is on the outside. Squared, wooden beams support the upper floor and the light fixtures attached to them. The walls are decorated with mounted animal heads and small animals, though most have become worn and broken, given the place an even creapier feel.
+
+The tavern itself is almost completely abanonded. The few people inside appear to be dangerous in one way or another, but whoever they are, it makes you a little nervous.
+
+You did hear rumors about this tavern, supposedly it's infamous for something, but for the life of you you can't remember what for. Though juding by judging by the people in this place, it's probably something shady and possibly dangerous for strangers like you.");
+            Console.WriteLine();
+            Console.WriteLine("From the shadows, a shrouded figure emerges...");
+            Console.WriteLine();
+            Console.WriteLine("Welcome, traveller...tell me, are you feeling very easy, medium, or hard today? Also definitely do NOT say \"cheater\"");
 
             string Difficulty = Console.ReadLine();
 
