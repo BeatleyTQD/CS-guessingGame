c907a802c869bccbf3e1290d4dca18b0054b9062
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-08-25 10:20:34 -0500

    Finished phase 2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace guessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int secretNumber = 42;
+            Console.Write("Weary traveller, what is the secret number?  ");
+            string answer = Console.ReadLine();
+            int answerNum = int.Parse(answer);
+
+            if (answerNum == secretNumber)
+            {
+                Console.WriteLine("Welcome aboard, agent");
+            }
+            else
+            {
+                Console.WriteLine("Guards, kill this intruder!");
+            }
         }
     }
 }
